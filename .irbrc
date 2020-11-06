0047021c60d166373e6fabc2f04abab4b4c57340
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2020-11-06 15:34:50 +0100

    migrated from rails-gke

--- .irbrc
+++ .irbrc
@@ -1,4 +1,21 @@
 require "souls"
+require "active_support/all"
+# require "yaml"
+# require "erb"
+# require "active_record"
+# require "logger"
+
+# $LOAD_PATH << "/Users/fumi/dev/souls/app/services"
+
+# Dir[File.expand_path "app/*.rb"].each do |file|
+#   require file
+# end
+
+# db_conf = YAML.safe_load(ERB.new(File.read("./config/database.yml")).result)
+# ActiveRecord::Base.establish_connection(db_conf)
+# Dir[File.expand_path "./app/controllers/*.rb"].sort.each do |file|
+#   require file
+# end
 require "yaml"
 require "erb"
 require "active_record"
--- config/initialize.rb
+++ config/initialize.rb
@@ -7,3 +7,33 @@ Souls.configure do |config|
   config.domain = "el-soul.com"
   config.google_application_credentials = "./config/credentials.json"
 end
+Souls.configure do |config|
+  config.project_id = "d"
+  config.app = "d"
+  config.network = "d"
+  config.machine_type = "d"
+  config.zone = "d"
+  config.domain = "d"
+  config.google_application_credentials = "d"
+  config.souls_mode = "API"
+end
+Souls.configure do |config|
+  config.project_id = "elsoul2"
+  config.app = "grpc-td-cluster"
+  config.network = "default"
+  config.machine_type = "custom-1-6656"
+  config.zone = "us-central1-a"
+  config.domain = "el-soul.com"
+  config.google_application_credentials = "./config/credentials.json"
+  config.souls_mode = "Service"
+end
+Souls.configure do |config|
+  config.project_id = "sdf"
+  config.app = "sdf"
+  config.network = "sdf"
+  config.machine_type = "sdf"
+  config.zone = "sdf"
+  config.domain = "sdf"
+  config.google_application_credentials = "sdf"
+  config.souls_mode = "Client"
+end
