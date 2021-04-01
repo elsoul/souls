34631e8cfb7d0102c16f33c4c1feed2e13ebddc1
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-04-01 23:38:40 +0900

    add rspec policy

--- lib/souls/generate.rb
+++ lib/souls/generate.rb
@@ -59,6 +59,50 @@ module Souls
         [file_path]
       end
 
+      def rspec_policy class_name: "souls"
+        dir_name = "./spec/policies"
+        FileUtils.mkdir_p dir_name unless Dir.exist? dir_name
+        file_path = "./spec/policies/#{class_name}_policy_spec.rb"
+        File.open(file_path, "w") do |new_line|
+          new_line.write <<~EOS
+            describe #{class_name.camelize}Policy do
+              subject { described_class.new(user, #{class_name.underscore}) }
+
+              let(:#{class_name.underscore}) { FactoryBot.create(:#{class_name.underscore}) }
+
+              context "being a visitor" do
+                let(:user) { FactoryBot.create(:user) }
+
+                it { is_expected.to permit_action(:index) }
+                it { is_expected.to permit_action(:show) }
+                it { is_expected.to forbid_actions([:create, :update, :delete]) }
+              end
+
+              context "being a retailer" do
+                let(:user) { FactoryBot.create(:user, user_role: 1) }
+
+                it { is_expected.to permit_action(:index) }
+                it { is_expected.to permit_action(:show) }
+                it { is_expected.to forbid_actions([:create, :update, :delete]) }
+              end
+
+              context "being a staff" do
+                let(:user) { FactoryBot.create(:user, user_role: 3) }
+
+                it { is_expected.to permit_actions([:create, :update, :delete]) }
+              end
+
+              context "being an administrator" do
+                let(:user) { FactoryBot.create(:user, user_role: 4) }
+
+                it { is_expected.to permit_actions([:create, :update, :delete]) }
+              end
+            end
+          EOS
+        end
+        [file_path]
+      end
+
       def resolver_head class_name: "souls"
         FileUtils.mkdir_p "./app/graphql/resolvers" unless Dir.exist? "./app/graphql/resolvers"
         file_path = "./app/graphql/resolvers/#{class_name.singularize}_search.rb"
--- lib/souls/version.rb
+++ lib/souls/version.rb
@@ -1,3 +1,3 @@
 module Souls
-  VERSION = "0.17.4"
+  VERSION = "0.17.5"
 end
