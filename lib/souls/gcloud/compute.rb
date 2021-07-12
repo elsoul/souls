77617e6f15fde7dada1b862af854d99962ee2e1f
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-07-12 15:12:40 +0200

    update permission

--- lib/souls/gcloud/compute.rb
+++ lib/souls/gcloud/compute.rb
@@ -4,12 +4,20 @@ module Souls
       def enable_permissions
         system "gcloud services enable compute.googleapis.com"
         puts "Operating permission to compute.googleapis.com ..."
+        system "gcloud services enable iam.googleapis.com"
+        puts "Operating permission to iam.googleapis.com ..."
+        system "gcloud services enable dns.googleapis.com"
+        puts "Operating permission to dns.googleapis.com ..."
         system "gcloud services enable sqladmin.googleapis.com"
         puts "Operating permission to sqladmin.googleapis.com ..."
         system "gcloud services enable sql-component.googleapis.com"
         puts "Operating permission to sql-component.googleapis.com ..."
         system "gcloud services enable servicenetworking.googleapis.com"
         puts "Operating permission to servicenetworking.googleapis.com ..."
+        system "gcloud services enable containerregistry.googleapis.com"
+        puts "Operating permission to containerregistry.googleapis.com"
+        system "gcloud services enable run.googleapis.com"
+        puts "Operating permission to run.googleapis.com"
       end
 
       def create_network
