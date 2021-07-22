e6e8368b1f0069fdb895e0582fc3be1eb4996c3d
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-07-22 13:55:56 +0200

    env

--- apps/worker/app/utils/fire_store.rb
+++ apps/worker/app/utils/fire_store.rb
@@ -2,7 +2,7 @@ module FireStore
   def self.log(title: "error", message: "type error!")
     Time.zone = "Asia/Tokyo"
     time = Time.zone.now.strftime("%F-%H-%M-%S")
-    firestore = Google::Cloud::Firestore.new(project_id: "usmef-japan-trade")
+    firestore = Google::Cloud::Firestore.new(project_id: ENV["project_id"])
     doc_ref = firestore.doc("Log/#{time}")
     doc_ref.set({ title: title, message: message, created_at: time })
   end
