10e5d5fbb91c9f6479af919e615a1444131d3681
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-08-09 18:47:55 +0200

    add update_pubsub_subscription

--- lib/souls/gcloud/pubsub.rb
+++ lib/souls/gcloud/pubsub.rb
@@ -21,6 +21,22 @@ module Souls
            "
         )
       end
+
+      def update_pubsub_subscription(
+        topic_name: "send-user-mail",
+        project_id: "souls-app",
+        service_account: "souls-app",
+        endpoint: "https:://test.com"
+      )
+        system(
+        "gcloud pubsub subscriptions update #{topic_name}-sub \
+        --topic #{topic_name} \
+        --topic-project #{project_id} \
+        --push-auth-service-account #{service_account} \
+        --push-endpoint #{endpoint} \
+        --expiration-period never
+         "
+        )
     end
   end
 end
