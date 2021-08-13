f97e831f7a9e4c1ec1fdea02e37ca584a1a24170
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-08-13 13:27:08 +0200

    add worker mailer

--- apps/worker/app/graphql/types/base/mutation_type.rb
+++ apps/worker/app/graphql/types/base/mutation_type.rb
@@ -8,5 +8,13 @@ module Types
       field worker.underscore.to_s.to_sym,
             mutation: Object.const_get("Mutations::Workers::#{worker.singularize.camelize}")
     end
+    mailers =
+      Dir["./app/graphql/mutations/mailers/*.rb"].map do |file|
+        file.gsub("./app/graphql/mutations/mailers/", "").gsub(".rb", "")
+      end
+    mailers.each do |mailer|
+      field mailer.underscore.to_s.to_sym,
+            mutation: Object.const_get("Mutations::Mailers::#{mailer.singularize.camelize}")
+    end
   end
 end
