98cd2d69c1dd93804d7707b1e3b045179e469e48
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-08-16 10:51:03 +0200

    fix migration command

--- lib/souls/api/generate/migration.rb
+++ lib/souls/api/generate/migration.rb
@@ -1,9 +1,36 @@
 module Souls
   module Api
-    module Migration
-      def self.create_migration(class_name: "user")
-        pluralized_class_name = class_name.underscore.pluralize
-        system("rake db:create_migration NAME=create_#{pluralized_class_name}")
+    module Generate
+      class << self
+        def create_migration(class_name: "user")
+          pluralized_class_name = class_name.underscore.pluralize
+          system("rake db:create_migration NAME=create_#{pluralized_class_name}")
+        end
+
+        def add_column(class_name: "user")
+          pluralized_class_name = class_name.underscore.pluralize
+          system("rake db:create_migration NAME=add_#{pluralized_class_name}")
+        end
+
+        def rename_column(class_name: "user")
+          pluralized_class_name = class_name.underscore.pluralize
+          system("rake db:create_migration NAME=rename_#{pluralized_class_name}")
+        end
+
+        def change_column(class_name: "user")
+          pluralized_class_name = class_name.underscore.pluralize
+          system("rake db:create_migration NAME=change_#{pluralized_class_name}")
+        end
+
+        def remove_column(class_name: "user")
+          pluralized_class_name = class_name.underscore.pluralize
+          system("rake db:create_migration NAME=remove_#{pluralized_class_name}")
+        end
+
+        def drop_table(class_name: "user")
+          pluralized_class_name = class_name.underscore.pluralize
+          system("rake db:create_migration NAME=drop_#{pluralized_class_name}")
+        end
       end
     end
   end
