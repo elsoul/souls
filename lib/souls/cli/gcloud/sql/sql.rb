9c860e45a71d7ccf178f08c934726f01a325a592
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-08-19 17:59:52 +0200

    fix gcloud sql

--- lib/souls/cli/gcloud/sql/sql.rb
+++ lib/souls/cli/gcloud/sql/sql.rb
@@ -3,9 +3,9 @@ module Souls
     module Sql
       class << self
         def create_instance(instance_name: "souls-db", root_pass: "Postgre123!", zone: "asia-northeast1-b")
-          system(
-            "gcloud sql instances create #{instance_name}
-              --database-version=POSTGRES_13 --cpu=2 --memory=7680MB --zone=#{zone}
+          puts(
+            "gcloud sql instances create #{instance_name} \
+              --database-version=POSTGRES_13 --cpu=2 --memory=7680MB --zone=#{zone} \
               --root-password='#{root_pass}' --database-flags cloudsql.iam_authentication=on"
           )
         end
