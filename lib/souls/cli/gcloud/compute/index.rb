cd466bb7bd2643383fe50ea2b0d66c3f1d4a9879
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-08-31 11:33:17 +0200

    add setup_vpc_nat

--- lib/souls/cli/gcloud/compute/index.rb
+++ lib/souls/cli/gcloud/compute/index.rb
@@ -5,11 +5,12 @@ module Souls
         def setup_vpc_nat(app_name: "", region: "asia-northeast1", range: "10.124.0.0/28")
           create_network(app_name: app_name)
           create_firewall_tcp(app_name: app_name, range: range)
-          create_firewall_ssh(app_name: app_name, range: range)
+          create_firewall_ssh(app_name: app_name)
           create_subnet(app_name: app_name, region: region, range: range)
           create_connector(app_name: app_name, region: region)
           create_external_ip(app_name: app_name, region: region)
           create_nat(app_name: app_name, region: region)
+          nat_credit(app_name: app_name)
         end
 
         def create_network(app_name: "")
@@ -70,6 +71,42 @@ module Souls
 
         def network_list
           system("gcloud compute network list")
+        end
+
+        def nat_credit(app_name: "", worker_name: "mailer")
+          app_name = Souls.configuration.app if app_name.blank?
+          line = Paint["====================================", :yellow]
+          puts("\n")
+          puts(line)
+          txt2 = <<~TEXT
+               _____ ____  __  ____#{'        '}
+              / ___// __ \\/ / / / /   %{red1}
+              \\__ \\/ / / / / / / /   %{red2}
+             ___/ / /_/ / /_/ / /___%{red3}#{' '}
+            /____/\\____/\\____/_____%{red4}#{'  '}
+          TEXT
+          red1 = ["_____", :red]
+          red2 = ["/ ___/", :red]
+          red3 = ["(__  )", :red]
+          red4 = ["/____/", :red]
+          ms = Paint % [txt2, :cyan, { red1: red1, red2: red2, red3: red3, red4: red4 }]
+          puts(ms)
+          puts(line)
+          welcome = Paint["VPC Network is All Set!", :white]
+          puts(welcome)
+          puts(line)
+          endroll = <<~TEXT
+
+            Edit  `.github/workflow/worker.yml`
+
+            Add these 2 options in `- name: Deploy to Cloud Run` step
+            --vpc-connector=#{app_name}-connector \
+            --vpc-egress=all \
+
+          TEXT
+          cd = Paint[endroll, :white]
+          puts(cd)
+          puts(line)
         end
       end
     end
