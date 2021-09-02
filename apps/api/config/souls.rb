c9c225acbe99c29287d43a47fe12a3ce41c24ebd
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-09-02 20:13:13 +0200

    update sync

--- apps/api/config/souls.rb
+++ apps/api/config/souls.rb
@@ -3,11 +3,5 @@ Souls.configure do |config|
   config.project_id = "souls-app"
   config.strain = "api"
   config.fixed_gems = [""]
-  config.workers = [
-    {
-      name: "mailer",
-      endpoint: "",
-      port: 3000
-    }
-  ]
+  config.workers = []
 end
