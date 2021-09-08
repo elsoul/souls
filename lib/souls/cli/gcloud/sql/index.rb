18fce611f60988367e730c6a10c24cd5b928c05d
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-09-08 13:20:01 +0200

    update sql

--- lib/souls/cli/gcloud/sql/index.rb
+++ lib/souls/cli/gcloud/sql/index.rb
@@ -4,9 +4,11 @@ module Souls
       class << self
         def create_instance(instance_name: "", root_pass: "Postgre123!", zone: "asia-northeast1-b")
           instance_name = "#{Souls.configuration.app}-db" if instance_name.blank?
+          current_ip = `curl inet-ip.info`
           system(
             "gcloud sql instances create #{instance_name} \
               --database-version=POSTGRES_13 --cpu=2 --memory=7680MB --zone=#{zone} \
+              --assign-ip=#{current_ip} \
               --root-password='#{root_pass}' --database-flags cloudsql.iam_authentication=on"
           )
         end
@@ -15,12 +17,13 @@ module Souls
           app_name = Souls.configuration.app
           instance_name = "#{Souls.configuration.app}-db" if instance_name.blank?
           project_id = Souls.configuration.project_id
-          system(
-            "gcloud beta sql instances patch #{instance_name} \
-              --project=#{project_id} \
-              --network=#{app_name} \
-              --no-assign-ip"
-          )
+          system("gcloud beta sql instances patch #{instance_name} --project=#{project_id} --network=#{app_name}")
+        end
+
+        def assign_ip
+          current_ip = `curl inet-ip.info`
+          project_id = Souls.configuration.project_id
+          system("gcloud beta sql instances patch #{instance_name} --project=#{project_id} --assign-ip=#{current_ip}")
         end
       end
     end
