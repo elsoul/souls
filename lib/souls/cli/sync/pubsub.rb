df90c78cddc50fd7319487fcba6ef64d3889a9ca
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-09-21 13:46:21 +0200

    update pubsub sync

--- lib/souls/cli/sync/pubsub.rb
+++ lib/souls/cli/sync/pubsub.rb
@@ -12,6 +12,7 @@ module Souls
     private
 
     def get_topics(workers: {})
+      app_name = Souls.configuration.app
       project_id = Souls.configuration.project_id
       pubsub = Google::Cloud::Pubsub.new
       topics = pubsub.topics
@@ -20,6 +21,8 @@ module Souls
         topics.map do |topic|
           topic.name.gsub("projects/#{project_id}/topics/", "")
         end
+
+      topic_names.select { |n| n.include?("#{app_name}_souls_") }
 
       topic_names.each do |name|
         value = workers[name.to_sym] || 0
@@ -37,23 +40,29 @@ module Souls
     end
 
     def create_topic(topic_id: "mailer")
+      app_name = Souls.configuration.app
       pubsub = Google::Cloud::Pubsub.new
-      topic = pubsub.create_topic(topic_id)
+      topic_name = "#{app_name}_souls_#{topic_id}"
+      topic = pubsub.create_topic(topic_name.to_s)
       puts("Topic #{topic.name} created.")
     end
 
     def delete_topic(topic_id: "mailer")
+      app_name = Souls.configuration.app
       pubsub = Google::Cloud::Pubsub.new
-      topic = pubsub.topic(topic_id)
+      topic_name = "#{app_name}_souls_#{topic_id}"
+      topic = pubsub.topic(topic_name.to_s)
       topic.delete
-      puts("Topic #{topic_id} deleted.")
+      puts("Topic #{topic_name} deleted.")
     end
 
     def create_push_subscription(topic_id: "mailer")
       require("#{Souls.get_mother_path}/config/souls")
       worker_name = topic_id.split("_")[0]
+      app_name = Souls.configuration.app
+      topic_name = "#{app_name}_souls_#{topic_id}"
 
-      subscription_id = "#{topic_id}_sub"
+      subscription_id = "#{topic_name}_sub"
       endpoint = ""
       worker_paths = Souls.configuration.workers
       worker_paths.each do |worker|
