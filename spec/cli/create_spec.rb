495b3174cbdf85096a45fbb49d507788d1f18ac9
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-09-21 21:50:38 +0200

    update create spec

--- spec/cli/create_spec.rb
+++ spec/cli/create_spec.rb
@@ -5,10 +5,12 @@ RSpec.describe(Souls::Create) do
       mother_tmp = "tmp/souls.rb"
       FileUtils.cp mother_conf, mother_tmp
       FileUtils.cp "apps/api/#{mother_conf}", "tmp/souls-api.rb"
+      FileUtils.cp "Procfile.dev", "tmp/Procfile.dev"
       a1 = Souls::Create.new.invoke(:worker, [], {name: "scraper"})
       expect(a1).to(eq(true))
       FileUtils.mv mother_tmp, mother_conf
       FileUtils.mv "tmp/souls-api.rb", "apps/api/#{mother_conf}"
+      FileUtils.mv "tmp/Procfile.dev", "Procfile.dev"
       FileUtils.rm_rf "apps/scraper"
       FileUtils.rm_rf ".github/workflows/scraper.yml"
     end
