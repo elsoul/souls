7d68ba981c4f907dd5782fb73f555bfe56fb1410
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-09-26 21:57:54 +0200

    fix job_rbs

--- lib/souls/worker/generate/job_rbs.rb
+++ lib/souls/worker/generate/job_rbs.rb
@@ -3,9 +3,10 @@ module Souls
     desc "job_rbs [CLASS_NAME]", "Generate SOULs Job Mutation RBS Template"
     def job_rbs(class_name)
       file_path = ""
+      worker_name = FileUtils.pwd.split("/").last
       Dir.chdir(Souls.get_mother_path.to_s) do
         singularized_class_name = class_name.underscore.singularize
-        file_dir = "./sig/#{singularized_class_name}/app/graphql/mutations/"
+        file_dir = "./sig/#{worker_name}/app/graphql/mutations/"
         FileUtils.mkdir_p(file_dir) unless Dir.exist?(file_dir)
         file_path = "#{file_dir}#{singularized_class_name}.rbs"
         File.open(file_path, "w") do |f|
