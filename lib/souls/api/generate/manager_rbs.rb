2c26e34f15d38349ce61fb2d1aaf88f541b71d24
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-09-27 15:13:39 +0200

    modify api g manager rbs

--- lib/souls/api/generate/manager_rbs.rb
+++ lib/souls/api/generate/manager_rbs.rb
@@ -5,24 +5,26 @@ module Souls
     def manager_rbs(class_name)
       file_path = ""
       singularized_class_name = class_name.underscore.singularize
-      file_dir = "./sig/api/app/graphql/mutations/managers/#{singularized_class_name}_manager"
-      FileUtils.mkdir_p(file_dir) unless Dir.exist?(file_dir)
-      file_path = "#{file_dir}/#{options[:mutation]}.rbs"
-      File.open(file_path, "w") do |f|
-        f.write(<<~TEXT)
-          module Mutations
-            module Managers
-              module #{singularized_class_name.camelize}Manager
-                class #{singularized_class_name.camelize}Login
-                  def self.description: (String)-> untyped
-                  def self.argument: (untyped, untyped, untyped)-> untyped
-                  def self.field: (untyped, untyped, untyped)-> untyped
+      Dir.chdir(Souls.get_mother_path.to_s) do
+        file_dir = "./sig/api/app/graphql/mutations/managers/#{singularized_class_name}_manager"
+        FileUtils.mkdir_p(file_dir) unless Dir.exist?(file_dir)
+        file_path = "#{file_dir}/#{options[:mutation]}.rbs"
+        File.open(file_path, "w") do |f|
+          f.write(<<~TEXT)
+            module Mutations
+              module Managers
+                module #{singularized_class_name.camelize}Manager
+                  class #{singularized_class_name.camelize}Login
+                    def self.description: (String)-> untyped
+                    def self.argument: (untyped, untyped, untyped)-> untyped
+                    def self.field: (untyped, untyped, untyped)-> untyped
+                  end
                 end
               end
             end
-          end
-        TEXT
-        puts(Paint % ["Created file! : %{white_text}", :green, { white_text: [file_path.to_s, :white] }])
+          TEXT
+          puts(Paint % ["Created file! : %{white_text}", :green, { white_text: [file_path.to_s, :white] }])
+        end
       end
       file_path
     rescue Thor::Error => e
