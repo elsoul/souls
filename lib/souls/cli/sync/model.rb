c3ab5fc706ba376bc5520088c0aecec6191f5ed7
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-10-11 10:43:52 +0200

    fix sync model

--- lib/souls/cli/sync/model.rb
+++ lib/souls/cli/sync/model.rb
@@ -15,7 +15,7 @@ module Souls
 
     def cp_and_dl_files(dir: "db")
       require("#{Souls.get_mother_path}/config/souls")
-      worker_paths = Souls.configuration.workers.map { |n| n[:name] }
+      worker_paths = Souls.configuration.workers.map { |n| n[:name].split("-").last }
       worker_paths.each do |path|
         cp_path = "./apps/api/#{dir}"
         old_path = "./apps/#{path}/#{dir}"
