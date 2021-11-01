fa8e300f4f5aa25e191d5d6c9baa165b6b41e1ad
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-11-01 18:19:54 +0100

    update gemfile version

--- lib/souls/cli/init/index.rb
+++ lib/souls/cli/init/index.rb
@@ -125,7 +125,7 @@ module Souls
           gem "pg", "1.2.3"
           gem "rake", "13.0.6"
           gem "rspec", "3.10.0"
-          gem "rubocop", "1.22.2"
+          gem "rubocop", "1.22.3"
           gem "sinatra-activerecord", "2.0.23"
           gem "solargraph", "0.44.0"
           gem "souls", "#{Souls::VERSION}"
