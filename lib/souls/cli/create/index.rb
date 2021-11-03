f0ac78a21a249260757756635fca8ea4f50acae7
James Neve <jamesoneve@gmail.com>
2021-11-03 14:53:54 +0100

    Test and refactor create index a little

--- lib/souls/cli/create/index.rb
+++ lib/souls/cli/create/index.rb
@@ -12,92 +12,86 @@ module Souls
         app = Souls.configuration.app
         port = 3000 + workers.size
         souls_worker_name = "souls-#{app}-#{options[:name]}"
-        download_worker(worker_name: options[:name])
-        souls_conf_update(worker_name: souls_worker_name)
-        souls_conf_update(worker_name: souls_worker_name, strain: "api")
-        workflow(worker_name: options[:name])
-        procfile(worker_name: options[:name], port: port)
-        mother_procfile(worker_name: options[:name])
-        souls_config_init(worker_name: options[:name])
-        steepfile(worker_name: options[:name])
-        souls_helper_rbs(worker_name: options[:name])
+        download_worker(options[:name])
+        souls_conf_update(souls_worker_name)
+        souls_conf_update(souls_worker_name, "api")
+        workflow(options[:name])
+        procfile(options[:name], port)
+        mother_procfile(options[:name])
+        souls_config_init(options[:name])
+        steepfile(options[:name])
+        souls_helper_rbs(options[:name])
         system("cd apps/#{options[:name]} && bundle")
-        souls_worker_credit(worker_name: options[:name])
+        souls_worker_credit(options[:name])
       end
       true
-    rescue Thor::Error => e
-      raise(Thor::Error, e)
     end
 
     private
 
-    def steepfile(worker_name: "mailer")
+    def steepfile(worker_name)
       file_path = "./Steepfile"
-      new_file_path = "config/Steepfile"
-      File.open(new_file_path, "w") do |new_line|
-        File.open(file_path, "r") do |f|
-          f.each_line do |line|
-            case line.strip.to_s
-            when "end"
-              ["app", "db/seeds.rb", "constants", "app.rb"].each do |path|
-                new_line.write("  check \"apps/#{worker_name}/#{path}\"\n")
-              end
-              new_line.write("end\n")
-            else
-              new_line.write(line)
+
+      write_txt = ""
+      File.open(file_path, "r") do |f|
+        f.each_line do |line|
+          if line.strip.to_s == "end"
+            ["app", "db/seeds.rb", "constants", "app.rb"].each do |path|
+              write_txt += "  check \"apps/#{worker_name}/#{path}\"\n"
             end
+            write_txt += "end\n"
+          else
+            write_txt += line
           end
         end
       end
-      FileUtils.rm(file_path)
-      FileUtils.mv(new_file_path, file_path)
+      File.open(file_path, "w") { |f| f.write(write_txt) }
     end
 
-    def procfile(worker_name: "mailer", port: 3000)
-      file_dir = "apps/#{worker_name}"
-      file_path = "#{file_dir}/Procfile.dev"
+    def procfile(worker_name, port)
+      file_path = "apps/#{worker_name}/Procfile.dev"
       File.open(file_path, "w") do |f|
         f.write("#{worker_name}: bundle exec puma -p #{port} -e development")
       end
     end
 
-    def mother_procfile(worker_name: "mailer")
+    def mother_procfile(worker_name)
       file_path = "Procfile.dev"
       File.open(file_path, "a") do |f|
         f.write("\n#{worker_name}: foreman start -f ./apps/#{worker_name}/Procfile.dev")
       end
     end
 
-    def souls_conf_update(worker_name: "", strain: "mother")
+    def souls_conf_update(worker_name, strain)
       workers = Souls.configuration.workers
       port = 3000 + workers.size
       file_path = strain == "mother" ? "config/souls.rb" : "apps/api/config/souls.rb"
-      new_file_path = "souls.rb"
-      worker_switch = false
-      File.open(new_file_path, "w") do |new_line|
-        File.open(file_path, "r") do |f|
-          f.each_line do |line|
-            worker_switch = true if line.include?("config.workers")
-            next if line.strip == "end"
 
-            new_line.write(line) unless worker_switch
+      write_txt = ""
+      File.open(file_path, "r") do |f|
+        f.each_line do |line|
+          worker_switch = line.include?("config.workers")
+          next if line.strip == "end"
 
-            next unless worker_switch
+          unless worker_switch
+            write_txt += line
+            next
+          end
 
-            new_line.write("  config.workers = [\n")
-            workers.each do |worker|
-              new_line.write(<<-TEXT)
+          write_txt += "  config.workers = [\n"
+          workers.each do |worker|
+            write_txt += <<-TEXT
     {
       name: "#{worker[:name]}",
       endpoint: "#{worker[:endpoint]}",
       port: #{worker[:port]}
     },
-              TEXT
-            end
-            break
+            TEXT
           end
+          break
         end
-        new_line.write(<<-TEXT)
+      end
+      write_txt += <<-TEXT
     {
       name: "#{worker_name}",
       endpoint: "",
@@ -105,13 +99,12 @@ module Souls
     }
   ]
 end
-        TEXT
-      end
-      FileUtils.rm(file_path)
-      FileUtils.mv(new_file_path, file_path)
+      TEXT
+
+      File.open(file_path, "w")  { |f| f.write(write_txt) }
     end
 
-    def workflow(worker_name: "")
+    def workflow(worker_name)
       file_dir = ".github/workflows"
       FileUtils.mkdir_p(file_dir) unless Dir.exist?(file_dir)
       file_path = "#{file_dir}/#{worker_name}.yml"
@@ -119,101 +112,99 @@ end
       worker_name_camelize = worker_name.camelize
       File.open(file_path, "w") do |f|
         f.write(<<~TEXT)
-            name: #{worker_name_camelize}
+          name: #{worker_name_camelize}
 
-            on:
-              push:
-                branches:
-                  - main
-                paths:
-                  - "apps/#{worker_name}/**"
-                  - ".github/workflows/#{worker_name}.yml"
+          on:
+            push:
+              branches:
+                - main
+              paths:
+                - "apps/#{worker_name}/**"
+                - ".github/workflows/#{worker_name}.yml"
 
-            jobs:
-              build:
+          jobs:
+            build:
 
-                runs-on: ubuntu-20.04
+              runs-on: ubuntu-20.04
 
-                services:
-                  db:
-                    image: postgres:13
-                    ports: ["5433:5432"]
-                    env:
-                      POSTGRES_PASSWORD: postgres
-                    options: >-
-                      --health-cmd pg_isready
-                      --health-interval 10s
-                      --health-timeout 5s
-                      --health-retries 5
-
-                steps:
-                - uses: actions/checkout@v2
-                - name: Set up Ruby 3.0
-                  uses: actions/setup-ruby@v1
-                  with:
-                    ruby-version: 3.0
-                - name: Build and test with Rake
+              services:
+                db:
+                  image: postgres:13
+                  ports: ["5433:5432"]
                   env:
-                    PGHOST: 127.0.0.1
-                    PGUSER: postgres
-                    RACK_ENV: test
-                  run: |
-                    sudo apt-get -yqq install libpq-dev
-                    cd apps/#{worker_name}
-                    gem install bundler
-                    bundle install --jobs 4 --retry 3
-                    bundle exec rake db:create RACK_ENV=test
-                    bundle exec rake db:migrate RACK_ENV=test
-                    bundle exec rspec
+                    POSTGRES_PASSWORD: postgres
+                  options: >-
+                    --health-cmd pg_isready
+                    --health-interval 10s
+                    --health-timeout 5s
+                    --health-retries 5
 
-                - name: Checkout the repository
-                  uses: actions/checkout@v2
+              steps:
+              - uses: actions/checkout@v2
+              - name: Set up Ruby 3.0
+                uses: actions/setup-ruby@v1
+                with:
+                  ruby-version: 3.0
+              - name: Build and test with Rake
+                env:
+                  PGHOST: 127.0.0.1
+                  PGUSER: postgres
+                  RACK_ENV: test
+                run: |
+                  sudo apt-get -yqq install libpq-dev
+                  cd apps/#{worker_name}
+                  gem install bundler
+                  bundle install --jobs 4 --retry 3
+                  bundle exec rake db:create RACK_ENV=test
+                  bundle exec rake db:migrate RACK_ENV=test
+                  bundle exec rspec
 
-                - name: GCP Authenticate
-                  uses: google-github-actions/setup-gcloud@master
-                  with:
-                    version: "323.0.0"
-                    project_id: ${{ secrets.SOULS_GCP_PROJECT_ID }}
-                    service_account_key: ${{ secrets.SOULS_GCP_SA_KEY }}
-                    export_default_credentials: true
+              - name: Checkout the repository
+                uses: actions/checkout@v2
 
-                - name: Configure Docker
-                  run: gcloud auth configure-docker --quiet
+              - name: GCP Authenticate
+                uses: google-github-actions/setup-gcloud@master
+                with:
+                  version: "323.0.0"
+                  project_id: ${{ secrets.SOULS_GCP_PROJECT_ID }}
+                  service_account_key: ${{ secrets.SOULS_GCP_SA_KEY }}
+                  export_default_credentials: true
 
-                - name: Build Docker container
-                  run: docker build -f ./apps/#{worker_name}/Dockerfile ./apps/#{worker_name} -t gcr.io/${{ secrets.SOULS_GCP_PROJECT_ID }}/${{secrets.SOULS_APP_NAME}}-#{worker_name}
+              - name: Configure Docker
+                run: gcloud auth configure-docker --quiet
 
-                - name: Push to Container Resistory
-                  run: docker push gcr.io/${{ secrets.SOULS_GCP_PROJECT_ID }}/${{secrets.SOULS_APP_NAME}}-#{worker_name}
+              - name: Build Docker container
+                run: docker build -f ./apps/#{worker_name}/Dockerfile ./apps/#{worker_name} -t gcr.io/${{ secrets.SOULS_GCP_PROJECT_ID }}/${{secrets.SOULS_APP_NAME}}-#{worker_name}
 
-                - name: Deploy to Cloud Run
-                  run: |
-                      gcloud run deploy souls-${{ secrets.SOULS_APP_NAME }}-#{worker_name} \\
-                        --service-account=${{ secrets.SOULS_APP_NAME }}@${{ secrets.SOULS_GCP_PROJECT_ID }}.iam.gserviceaccount.com \\
-                        --image=gcr.io/${{ secrets.SOULS_GCP_PROJECT_ID }}/${{secrets.SOULS_APP_NAME}}-#{worker_name} \\
-                        --memory=4Gi \\
-                        --region=${{ secrets.SOULS_GCP_REGION }} \\
-                        --allow-unauthenticated \\
-                        --platform=managed \\
-                        --quiet \\
-                        --concurrency=80 \\
-                        --port=8080 \\
-                        --set-cloudsql-instances=${{ secrets.SOULS_GCLOUDSQL_INSTANCE }} \\
-                        --set-env-vars="SOULS_DB_USER=${{ secrets.SOULS_DB_USER }}" \\
-                        --set-env-vars="SOULS_DB_PW=${{ secrets.SOULS_DB_PW }}" \\
-                        --set-env-vars="SOULS_DB_HOST=${{ secrets.SOULS_DB_HOST }}" \\
-                        --set-env-vars="SOULS_TZ=${{ secrets.SOULS_TZ }}" \\
-                        --set-env-vars="SOULS_SECRET_KEY_BASE=${{ secrets.SOULS_SECRET_KEY_BASE }}" \\
-                        --set-env-vars="SOULS_PROJECT_ID=${{ secrets.SOULS_GCP_PROJECT_ID }}"
+              - name: Push to Container Resistory
+                run: docker push gcr.io/${{ secrets.SOULS_GCP_PROJECT_ID }}/${{secrets.SOULS_APP_NAME}}-#{worker_name}
+
+              - name: Deploy to Cloud Run
+                run: |
+                    gcloud run deploy souls-${{ secrets.SOULS_APP_NAME }}-#{worker_name} \\
+                      --service-account=${{ secrets.SOULS_APP_NAME }}@${{ secrets.SOULS_GCP_PROJECT_ID }}.iam.gserviceaccount.com \\
+                      --image=gcr.io/${{ secrets.SOULS_GCP_PROJECT_ID }}/${{secrets.SOULS_APP_NAME}}-#{worker_name} \\
+                      --memory=4Gi \\
+                      --region=${{ secrets.SOULS_GCP_REGION }} \\
+                      --allow-unauthenticated \\
+                      --platform=managed \\
+                      --quiet \\
+                      --concurrency=80 \\
+                      --port=8080 \\
+                      --set-cloudsql-instances=${{ secrets.SOULS_GCLOUDSQL_INSTANCE }} \\
+                      --set-env-vars="SOULS_DB_USER=${{ secrets.SOULS_DB_USER }}" \\
+                      --set-env-vars="SOULS_DB_PW=${{ secrets.SOULS_DB_PW }}" \\
+                      --set-env-vars="SOULS_DB_HOST=${{ secrets.SOULS_DB_HOST }}" \\
+                      --set-env-vars="SOULS_TZ=${{ secrets.SOULS_TZ }}" \\
+                      --set-env-vars="SOULS_SECRET_KEY_BASE=${{ secrets.SOULS_SECRET_KEY_BASE }}" \\
+                      --set-env-vars="SOULS_PROJECT_ID=${{ secrets.SOULS_GCP_PROJECT_ID }}"
         TEXT
       end
       puts(Paint % ["Created file! : %{white_text}", :green, { white_text: [file_path.to_s, :white] }])
       file_path
-    rescue StandardError => e
-      raise(StandardError, e)
     end
 
-    def souls_config_init(worker_name: "mailer")
+    def souls_config_init(worker_name)
       app_name = Souls.configuration.app
       project_id = Souls.configuration.project_id
       config_dir = "apps/#{worker_name}/config"
@@ -222,22 +213,22 @@ end
       file_path = "#{config_dir}/souls.rb"
       File.open(file_path, "w") do |f|
         f.write(<<~TEXT)
-            Souls.configure do |config|
-              config.app = "#{app_name}"
-              config.project_id = "#{project_id}"
-              config.region = "asia-northeast1"
-              config.endpoint = "/endpoint"
-              config.strain = "worker"
-              config.fixed_gems = ["spring"]
-              config.workers = []
-            end
+          Souls.configure do |config|
+            config.app = "#{app_name}"
+            config.project_id = "#{project_id}"
+            config.region = "asia-northeast1"
+            config.endpoint = "/endpoint"
+            config.strain = "worker"
+            config.fixed_gems = ["spring"]
+            config.workers = []
+          end
         TEXT
       end
     rescue StandardError => e
       puts(e)
     end
 
-    def souls_helper_rbs(worker_name: "mailer")
+    def souls_helper_rbs(worker_name)
       file_dir = "./sig/#{worker_name}/app/utils/"
       FileUtils.mkdir_p(file_dir) unless Dir.exist?(file_dir)
       file_path = "#{file_dir}/souls_helper.rbs"
@@ -278,7 +269,7 @@ end
       end
     end
 
-    def download_worker(worker_name: "mailer")
+    def download_worker(worker_name)
       version = Souls.get_latest_version_txt(service_name: "worker").join(".")
       file_name = "worker-v#{version}.tgz"
       url = "https://storage.googleapis.com/souls-bucket/boilerplates/workers/#{file_name}"
@@ -289,7 +280,7 @@ end
       FileUtils.rm(file_name)
     end
 
-    def souls_worker_credit(worker_name: "mailer")
+    def souls_worker_credit(worker_name)
       line = Paint["====================================", :yellow]
       puts("\n")
       puts(line)
---
+++ spec/cli/create/scaffolds/scaffold_souls.rb
@@ -0,0 +1,35 @@
+module Scaffold
+  def self.scaffold_souls
+    <<~SOULS
+Souls.configure do |config|
+  config.app = "souls"
+  config.project_id = "el-quest"
+  config.region = "asia-northeast1"
+  config.endpoint = "/endpoint"
+  config.strain = "mother"
+  config.fixed_gems = ["spring"]
+  config.workers = []
+end
+    SOULS
+  end
+
+  def self.scaffold_souls_updated
+    <<~SOULSUPDATED
+Souls.configure do |config|
+  config.app = "souls"
+  config.project_id = "el-quest"
+  config.region = "asia-northeast1"
+  config.endpoint = "/endpoint"
+  config.strain = "mother"
+  config.fixed_gems = ["spring"]
+  config.workers = [
+    {
+      name: "mailer",
+      endpoint: "",
+      port: 3000
+    }
+  ]
+end
+    SOULSUPDATED
+  end
+end
