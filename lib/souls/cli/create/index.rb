42099d956dabc63ea1ec9bb1801e3b3d0b2bdc62
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-11-09 18:47:30 +0100

    update workflows

--- lib/souls/cli/create/index.rb
+++ lib/souls/cli/create/index.rb
@@ -145,6 +145,7 @@ end
                 uses: actions/setup-ruby@v1
                 with:
                   ruby-version: 3.0
+                  bundler-cache: true
               - name: Build and test with Rake
                 env:
                   PGHOST: 127.0.0.1
