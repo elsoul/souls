5c68dde9cbfb7d8b4d95ddb032ee8f5df98c06d8
James Neve <jamesoneve@gmail.com>
2021-11-11 11:22:04 +0100

    Change jobs from mutations to queries, remove queries from workers

--- spec/cli/generate/job_spec.rb
+++ spec/cli/generate/job_spec.rb
@@ -1,29 +1,111 @@
 require_relative "./output_scaffolds/scaffold_job"
 
 RSpec.describe(Souls::Generate) do
-  describe "Generate Manager" do
-    let(:class_name) { "user" }
+  describe "Generate Job" do
+    let(:class_name) { "some_job" }
 
-    before do
-      FakeFS do
-        @file_dir = "./app/graphql/mutations/"
-        FileUtils.mkdir_p(@file_dir) unless Dir.exist?(@file_dir)
+    it "creates job file" do
+      file_scaffold = OutputScaffold.scaffold_job
+
+      FakeFS.with_fresh do
+        query_dir = "./app/graphql/queries/"
+        FileUtils.mkdir_p(query_dir)
+
+        file_path = "#{query_dir}#{class_name.singularize}.rb"
+
+        generate = Souls::Generate.new
+        a1 = generate.send(:create_job, class_name)
+        file_output = File.read(file_path)
+
+        expect(a1).to(eq(file_path))
+        expect(File.exists? file_path).to(eq(true))
+
+        expect(file_output).to(eq(file_scaffold))
       end
     end
 
-    it "creates job file" do
-      file_path = "#{@file_dir}#{class_name.singularize}.rb"
-      FakeFS.activate!
-      generate = Souls::Generate.new
-      allow(Souls).to receive(:get_mother_path).and_return("")
-      a1 = generate.send(:create_job_mutation, class_name)
-      file_output = File.read(file_path)
+    it "creates type file" do
+      file_scaffold = OutputScaffold.scaffold_job_type
 
-      expect(a1).to(eq(file_path))
-      expect(File.exists? file_path).to(eq(true))
-      FakeFS.deactivate!
+      FakeFS.with_fresh do
+        type_dir = "./app/graphql/types/"
+        FileUtils.mkdir_p(type_dir)
 
-      expect(file_output).to(eq(OutputScaffold.scaffold_job))
+          file_path = "#{type_dir}#{class_name.singularize}_type.rb"
+
+        generate = Souls::Generate.new
+        a1 = generate.send(:create_job_type, class_name)
+        file_output = File.read(file_path)
+
+        expect(a1).to(eq(file_path))
+        expect(File.exists? file_path).to(eq(true))
+
+        expect(file_output).to(eq(file_scaffold))
+      end
+    end
+
+    it "creates mailer with correct argument" do
+      file_scaffold = OutputScaffold.scaffold_job_mailer
+
+      FakeFS.with_fresh do
+        query_dir = "./app/graphql/queries/"
+        FileUtils.mkdir_p(query_dir)
+
+        file_path = "#{query_dir}#{class_name.singularize}.rb"
+
+        generate = Souls::Generate.new
+        a1 = generate.send(:mailgun_mailer, class_name)
+        file_output = File.read(file_path)
+
+        expect(a1).to(eq(file_path))
+        expect(File.exists? file_path).to(eq(true))
+
+        expect(file_output).to(eq(file_scaffold))
+      end
+    end
+
+    it "creates mailer type file" do
+      file_scaffold = OutputScaffold.scaffold_job_mailer_type
+
+      FakeFS.with_fresh do
+        type_dir = "./app/graphql/types/"
+        FileUtils.mkdir_p(type_dir)
+
+        file_path = "#{type_dir}#{class_name.singularize}_type.rb"
+
+        generate = Souls::Generate.new
+        allow(Souls).to receive(:get_mother_path).and_return("")
+        a1 = generate.send(:create_job_mailer_type, class_name)
+        file_output = File.read(file_path)
+
+        expect(a1).to(eq(file_path))
+        expect(File.exists? file_path).to(eq(true))
+
+        expect(file_output).to(eq(file_scaffold))
+      end
+    end
+
+    it "adds appropriate routes" do
+      base_query_type = OutputScaffold.base_query_type
+      modified_query_type = OutputScaffold.modified_query_type
+
+      FakeFS.with_fresh do
+        type_dir = "./app/graphql/types/base/"
+        FileUtils.mkdir_p(type_dir)
+
+        file_path = "#{type_dir}query_type.rb"
+        File.open(file_path, "w") { |f| f.write(base_query_type) }
+        expect(File.read(file_path)).to(eq(base_query_type))
+
+        generate = Souls::Generate.new
+        a1 = generate.send(:update_query_type, class_name)
+        file_output = File.read(file_path)
+
+        expect(a1).to(eq(file_path))
+        expect(File.exists? file_path).to(eq(true))
+
+        expect(file_output).to(eq(modified_query_type))
+      end
     end
   end
 end
