16eaa3673af6d82eb99ee79b2301ada9c7ffd742
James Neve <jamesoneve@gmail.com>
2021-11-16 12:15:40 +0100

    Commit for locacl release

--- lib/souls/cli/cli_exception.rb
+++ lib/souls/cli/cli_exception.rb
@@ -6,5 +6,9 @@ module Souls
       super
       @message = message
     end
+
+    def psql_exception
+      initialize("It looks like there was a problem with the DB. Make sure PSQL is running with 'souls docker psql'")
+    end
   end
 end
