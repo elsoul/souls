2f322fff9b9613fa752553b075f4035faa64e3d6
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-11-17 13:26:57 +0100

    update gh add_env

--- lib/souls/cli/github/index.rb
+++ lib/souls/cli/github/index.rb
@@ -2,23 +2,27 @@ module Souls
   class Github < Thor
     desc "secret_set", "Github Secret Set from .env.production"
     def secret_set
-      file_path = ".env.production"
-      File.open(file_path, "r") do |file|
-        file.each_line do |line|
-          key_and_value = line.split("=")
-          system("gh secret set #{key_and_value[0]} -b \"#{key_and_value[1].strip}\"")
+      Dir.chdir(Souls.get_mother_path.to_s) do
+        file_path = ".env.production"
+        File.open(file_path, "r") do |file|
+          file.each_line do |line|
+            key_and_value = line.split("=")
+            system("gh secret set #{key_and_value[0]} -b \"#{key_and_value[1].strip}\"")
+          end
         end
       end
     end
 
     desc "add_env", "Add New env and Sync Github Secret"
-    method_option :key, aliases: "--key", required: true, desc: "Key Name"
-    method_option :value, aliases: "--value", required: true, desc: "Value Name"
     method_option :dqm, type: :boolean, aliases: "--dqm", default: false, desc: "Enable Double Quotation Mark"
     def add_env
-      update_env_production(key: options[:key], value: options[:value], dqm: options[:dqm])
-      update_api_env(key: options[:key], value: options[:value], dqm: options[:dqm])
-      update_github_actions(key: options[:key])
+      prompt = TTY::Prompt.new
+      key = prompt.ask("Set Key:")
+      value = prompt.ask("Set Value:")
+      update_env_production(key: key, value: value, dqm: options[:dqm])
+      update_api_env(key: key, value: value, dqm: options[:dqm])
+      update_workers_env(key: key, value: value, dqm: options[:dqm])
+      update_github_actions(key: key)
       Souls::Github.new.invoke(:secret_set)
     end
 
@@ -49,6 +53,20 @@ module Souls
           dqm ? line.write("\n#{key.upcase}=\"#{value}\"") : line.write("\n#{key.upcase}=#{value}")
         end
         puts(Paint % ["Updated file! : %{white_text}", :green, { white_text: [file_path.to_s, :white] }])
+      end
+    end
+
+    def update_workers_env(key:, value:, dqm: false)
+      Dir.chdir(Souls.get_mother_path.to_s) do
+        workers = Dir["apps/*"]
+        workers.delete("apps/api")
+        workers.each do |worker_path|
+          file_path = "#{worker_path}/.env"
+          File.open(file_path, "a") do |line|
+            dqm ? line.write("\n#{key.upcase}=\"#{value}\"") : line.write("\n#{key.upcase}=#{value}")
+          end
+          puts(Paint % ["Updated file! : %{white_text}", :green, { white_text: [file_path.to_s, :white] }])
+        end
       end
     end
 
