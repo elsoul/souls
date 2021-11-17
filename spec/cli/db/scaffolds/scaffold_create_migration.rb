f575383fe6a5ddcd962e7b0ce0f765d232266865
James Neve <jamesoneve@gmail.com>
2021-11-17 11:36:04 +0100

    Add remaining tests for DB directory

---
+++ spec/cli/db/scaffolds/scaffold_create_migration.rb
@@ -0,0 +1,15 @@
+module Scaffold
+  def self.scaffold_create_migration
+    <<~CREATEMIGRATION
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+
+      t.boolean :is_deleted, null: false, default: false
+      t.timestamps
+    end
+  end
+end
+CREATEMIGRATION
+  end
+end
