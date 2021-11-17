42d2461c389d83e73a25969124dbec11f3f33aba
James Neve <jamesoneve@gmail.com>
2021-11-17 17:27:58 +0100

    Copy env files

--- lib/souls/cli/sync/model.rb
+++ lib/souls/cli/sync/model.rb
@@ -9,6 +9,7 @@ module Souls
       cp_dir = %w[db app/models spec/factories]
       cp_dir.each do |dir|
         cp_and_dl_files(dir: dir)
+        cp_env_files
       end
       puts(Paint % ["Synced! : %{white_text}", :green, { white_text: [cp_dir.to_s, :white] }])
     rescue Thor::Error => e
@@ -23,11 +24,22 @@ module Souls
       worker_paths.each do |path|
         cp_path = "./apps/api/#{dir}"
         old_path = "./apps/#{path}/#{dir}"
+
         system("rm -rf #{old_path}", chdir: Souls.get_mother_path)
         system("mkdir -p #{old_path}", chdir: Souls.get_mother_path)
         system("cp -r #{cp_path}/* #{old_path}", chdir: Souls.get_mother_path)
       rescue StandardError
         # Do nothing
+      end
+    end
+
+    def cp_env_files
+      return unless system("cat ./apps/api/.env", out: File::NULL, chdir: Souls.get_mother_path)
+
+      worker_paths = Souls.configuration.workers.map { |n| n[:name].split("-").last }
+      worker_paths.each do |path|
+        system("rm ./apps/#{path}/.env", chdir: Souls.get_mother_path)
+        system("cp ./apps/api/.env #{apps}/path/.env", chdir: Souls.get_mother_path)
       end
     end
   end
