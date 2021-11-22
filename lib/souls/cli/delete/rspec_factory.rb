e6c78b65aaa24b4abdbd60f7eafcf19b24ffc1ad
James Neve <jamesoneve@gmail.com>
2021-11-22 11:46:54 +0100

    Add tests for delete, and do some refactoring

--- lib/souls/cli/delete/rspec_factory.rb
+++ lib/souls/cli/delete/rspec_factory.rb
@@ -3,7 +3,7 @@ module Souls
     desc "rspec_factory [CLASS_NAME]", "Delete Rspec Factory Test from schema.rb"
     def rspec_factory(class_name)
       file_path = "./spec/factories/#{class_name.pluralize}.rb"
-      FileUtils.rm(file_path)
+      FileUtils.rm_f(file_path)
       puts(Paint % ["Deleted file! : %{white_text}", :yellow, { white_text: [file_path.to_s, :white] }])
       file_path
     end
--- lib/souls/cli/delete/rspec_job.rb
+++ lib/souls/cli/delete/rspec_job.rb
@@ -2,9 +2,10 @@ module Souls
   class Delete < Thor
     desc "rspec_job [CLASS_NAME]", "Delete Rspec Job Test Template"
     def rspec_job(class_name)
+      file_dir = "./spec/jobs"
       singularized_class_name = class_name.underscore.singularize
       file_path = "#{file_dir}/#{singularized_class_name}_spec.rb"
-      FileUtils.rm(file_path)
+      FileUtils.rm_f(file_path)
       puts(Paint % ["Deleted file! : %{white_text}", :yellow, { white_text: [file_path.to_s, :white] }])
       file_path
     end
