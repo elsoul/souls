c329bf345261eaadab7618b224f1b56b3f9b1725
James Neve <jamesoneve@gmail.com>
2021-11-24 16:13:12 +0100

    Set timezone

--- lib/souls/cli/gcloud/scheduler/index.rb
+++ lib/souls/cli/gcloud/scheduler/index.rb
@@ -18,7 +18,7 @@ module Souls
         system("gcloud scheduler jobs delete #{job_name} -q >/dev/null 2>&1")
         system(
           <<~COMMAND)
-            gcloud scheduler jobs create pubsub #{job_name} --schedule="#{v}" --topic="#{k}" --attributes="" --message-body="#{k}"
+            gcloud scheduler jobs create pubsub #{job_name} --schedule="#{v}" --topic="#{k}" --attributes="" --message-body="#{k}" --time-zone="#{ENV['SOULS_TZ']}"
           COMMAND
       end
     end
