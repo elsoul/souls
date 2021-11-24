10343da5794543f56b73a2da39016cb2552782eb
James Neve <jamesoneve@gmail.com>
2021-11-24 13:21:51 +0100

    Get from cloud scheduler

--- lib/souls/cli/gcloud/scheduler/index.rb
+++ lib/souls/cli/gcloud/scheduler/index.rb
@@ -1,3 +1,5 @@
+require "google/cloud/scheduler/v1"
+
 module Souls
   class CloudScheduler < Thor
     desc "awake", "Set Ping Every 15min by Google Cloud Scheduler"
@@ -13,7 +15,11 @@ module Souls
     desc "sync_schedules", "Collect schedules from queries and sync with GCloud"
     def sync_schedules
       require("./app")
-      puts(Queries::BaseQuery.all_schedules)
+
+      client = ::Google::Cloud::Scheduler::V1::CloudScheduler::Client.new
+      request = ::Google::Cloud::Scheduler::V1::ListJobsRequest.new
+      response = client.list_jobs(request)
+      puts(response)
     end
   end
 end
