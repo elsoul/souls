6b073db205d94506fd6eefcbc3b35f96f6152f10
James Neve <jamesoneve@gmail.com>
2021-11-24 12:49:16 +0100

    Try sync schedules

---
+++ lib/souls/cli/gcloud/scheduler/index.rb
@@ -0,0 +1,18 @@
+module Souls
+  class CloudScheduler < Thor
+    desc "awake", "Set Ping Every 15min by Google Cloud Scheduler"
+    method_option :url, default: "https://souls.souls.nl", aliases: "--url", desc: "Set URL"
+    def awake
+      app_name = Souls.configuration.app
+      system(
+        "gcloud scheduler jobs create http #{app_name}-awake
+            --schedule '0,10,20,30,40,50 * * * *' --uri #{options[:url]} --http-method GET"
+      )
+    end
+
+    desc "sync schedules", "Collect schedules from queries and sync with GCloud"
+    def sync_schedules
+      puts(Queries::BaseQuery.all_schedules)
+    end
+  end
+end
