f40ebbd49d2854eeb54efd3685ba499318b6dfca
James Neve <jamesoneve@gmail.com>
2021-11-25 15:22:16 +0100

    Add SoulsQuery

--- apps/worker/app/graphql/queries/base_query.rb
+++ apps/worker/app/graphql/queries/base_query.rb
@@ -1,24 +1,4 @@
 module Queries
-  class BaseQuery < GraphQL::Schema::Resolver
-    @schedule = nil
-    class << self
-      attr_accessor :schedule
-    end
-
-    def self.cron(schedule)
-      self.schedule = schedule
-    end
-
-    def self.all_schedules
-      schedule_list = {}
-      Queries.constants.select { |c| Queries.const_get(c).is_a?(Class) }
-             .each do |clname|
-        next if clname == :BaseQuery
-
-        job_schedule = Queries.const_get(clname).schedule
-        schedule_list[clname] = job_schedule unless job_schedule.nil?
-      end
-      schedule_list
-    end
+  class BaseQuery < Souls::SoulsQuery
   end
 end
---
+++ spec/app/graphql/souls_query_spec.rb
@@ -0,0 +1,36 @@
+module Queries
+  class ExampleQuery < Souls::SoulsQuery
+    cron "1 0 * * *"
+    def resolve
+      true
+    end
+  end
+
+  class ExampleQuery2 < Souls::SoulsQuery
+    def resolve
+      true
+    end
+  end
+
+  class ExampleQuery3 < Souls::SoulsQuery
+    cron "1 5 * * *"
+    def resolve
+      true
+    end
+  end
+end
+
+RSpec.describe(Souls::SoulsQuery) do
+  describe "self.cron" do
+    it "should set schedule for children" do
+      expect(Queries::ExampleQuery.schedule).to(eq("1 0 * * *"))
+    end
+  end
+
+  describe "self.all_schedules" do
+    it "should get all schedules that have cron" do
+      expected = { ExampleQuery: "1 0 * * *", ExampleQuery3: "1 5 * * *" }
+      expect(Queries::ExampleQuery.all_schedules).to(eq(expected))
+    end
+  end
+end
