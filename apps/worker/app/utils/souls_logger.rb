869f49c00278d60c7e8ee3202aebf03c3ba8b6ee
James Neve <jamesoneve@gmail.com>
2021-11-29 16:22:37 +0100

    Refactor logger to gem

--- apps/worker/app/utils/souls_logger.rb
+++ apps/worker/app/utils/souls_logger.rb
@@ -1,35 +1,3 @@
-module SoulsLogger
-  @logger = Google::Cloud::Logging.new(project_id: ENV["SOULS_GCP_PROJECT_ID"])
-
-  def self.critical_log(message)
-    entry = write_log(message)
-    entry.critical!
-
-    @logger.write_entries(entry)
-  end
-
-  def self.warning_log(message)
-    entry = write_log(message)
-    entry.warning!
-
-    @logger.write_entries(entry)
-  end
-
-  def self.info_log(message)
-    entry = write_log(message)
-    entry.info!
-
-    @logger.write_entries(entry)
-  end
-
-  def self.write_log(message)
-    entry = @logger.entry
-    entry.payload = "#{message}\n #{message.backtrace.join("\n")}"
-    entry.log_name = "error"
-    entry.resource.type = "cloud_run_revision"
-    entry.resource.labels[:service_name] = "souls"
-    entry.resource.labels[:revision_name] = Souls::VERSION
-
-    entry
-  end
+Souls::SoulsLogger.configure do |config|
+  config.logger = Google::Cloud::Logging.new(project_id: ENV["SOULS_GCP_PROJECT_ID"])
 end
