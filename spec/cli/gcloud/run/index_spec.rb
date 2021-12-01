a1738ff4abaded7ea1b5270070851ed40ba6f69b
James Neve <jamesoneve@gmail.com>
2021-12-01 16:40:28 +0100

    Add tests for gcloud run

---
+++ spec/cli/gcloud/run/index_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe(Souls::CloudRun) do
+  describe "list" do
+    it "should call gcloud" do
+      cli = Souls::CloudRun.new
+      allow(cli).to(receive(:system).and_return(true))
+      expect(cli).to(receive(:system).with("gcloud run services list --platform managed"))
+
+      expect(cli.list).to(eq(true))
+    end
+  end
+
+  describe "get_endpoint" do
+    it "should call gcloud" do
+      cli = Souls::CloudRun.new
+      allow(cli).to(receive(:`).and_return(true))
+      expect(cli).to(receive(:`).with("gcloud run services list  --platform managed | grep worker | awk '{print $4}'"))
+
+      expect(cli.get_endpoint(worker_name: "worker")).to(eq(true))
+    end
+  end
+end
