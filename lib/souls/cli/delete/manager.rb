973ebfbdd2936b8cd33318c1f6e80ad31e06d654
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-12-08 13:49:49 +0100

    fix souls d manager

--- lib/souls/cli/delete/manager.rb
+++ lib/souls/cli/delete/manager.rb
@@ -6,10 +6,9 @@ module Souls
       singularized_class_name = class_name.underscore.singularize
       file_dir = "./app/graphql/mutations/managers/#{singularized_class_name}_manager"
       file_path = "#{file_dir}/#{options[:mutation]}.rb"
-      puts(file_path)
 
       FileUtils.rm_f(file_path)
-      puts(Paint % ["Created file! : %{white_text}", :green, { white_text: [file_path.to_s, :white] }])
+      puts(Paint % ["Deleted file! : %{white_text}", :yellow, { white_text: [file_path.to_s, :white] }])
       Souls::Delete.new.invoke(:manager_rbs, [singularized_class_name], { mutation: options[:mutation] })
       Souls::Delete.new.invoke(:rspec_manager, [singularized_class_name], { mutation: options[:mutation] })
       file_path
