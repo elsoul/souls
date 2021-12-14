7f5f9b17a9b845328dcf4b8e4ae6556b0aebdd8b
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-12-14 17:05:11 +0100

    fix job rbs spec

--- lib/souls/cli/delete/rspec_job.rb
+++ lib/souls/cli/delete/rspec_job.rb
@@ -2,7 +2,7 @@ module Souls
   class Delete < Thor
     desc "rspec_job [CLASS_NAME]", "Delete Rspec Job Test Template"
     def rspec_job(class_name)
-      file_dir = "./spec/jobs"
+      file_dir = "./spec/queries/jobs"
       singularized_class_name = class_name.underscore.singularize
       file_path = "#{file_dir}/#{singularized_class_name}_spec.rb"
       FileUtils.rm_f(file_path)
