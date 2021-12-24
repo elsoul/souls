03143d3a414856c6c6901d00c4562a217309794c
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-12-24 19:00:16 +0100

    add souls functions

---
+++ lib/souls/cli/create/functions.rb
@@ -0,0 +1,35 @@
+require_relative "./template/functions_app"
+require_relative "./template/functions_gemfile"
+module Souls
+  class Create < Thor
+    desc "functions", "Create SOULs functions"
+    def functions
+      create_app_file
+      create_gemfile
+    end
+
+    private
+
+    def create_app_file
+      file_dir = "./apps/functions"
+      FileUtils.mkdir_p(file_dir) unless Dir.exist?(file_dir)
+      file_path = "#{file_dir}/app.rb"
+      raise(StandardError, "Already Exist!") if File.exist?(file_path)
+
+      File.write(file_path, Template.functions_app)
+      Souls::Painter.create_file(file_path)
+      file_path
+    end
+
+    def create_gemfile
+      file_dir = "./apps/functions"
+      FileUtils.mkdir_p(file_dir) unless Dir.exist?(file_dir)
+      file_path = "#{file_dir}/Gemfile"
+      raise(StandardError, "Already Exist!") if File.exist?(file_path)
+
+      File.write(file_path, Template.functions_gemfile)
+      Souls::Painter.create_file(file_path)
+      file_path
+    end
+  end
+end
---
+++ lib/souls/cli/gcloud/functions/index.rb
@@ -0,0 +1,24 @@
+module Souls
+  class Functions < Thor
+    desc "deploy", "Deploy Cloud Functions"
+    def deploy
+      project_id = Souls.configuration.project_id
+      Souls::Gcloud.new.config_set
+      Dir.chdir(Souls.get_functions_path.to_s) do
+        system(
+          "
+          gcloud functions deploy souls_functions --project=#{project_id} \
+          --runtime ruby27 --trigger-http --allow-unauthenticated
+          "
+        )
+      end
+    end
+
+    desc "dev", "Check SOULs Functions dev"
+    def dev
+      Dir.chdir(Souls.get_functions_path.to_s) do
+        system("bundle exec functions-framework-ruby --target souls_functions")
+      end
+    end
+  end
+end
