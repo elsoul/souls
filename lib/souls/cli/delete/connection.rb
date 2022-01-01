1b8bec608d97c9b3044d550b6c44e91d7ac8bf08
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2022-01-01 11:56:12 +0100

    add Souls::Painter.delete_file

--- lib/souls/cli/delete/connection.rb
+++ lib/souls/cli/delete/connection.rb
@@ -6,7 +6,7 @@ module Souls
       singularized_class_name = class_name.underscore.singularize
       file_path = "#{file_dir}#{singularized_class_name}_connection.rb"
       FileUtils.rm_f(file_path)
-      puts(Paint % ["Delete file! : %{white_text}", :yellow, { white_text: [file_path.to_s, :white] }])
+      Souls::Painter.delete_file(file_path)
       file_path
     end
   end
