b7b0924c2db9a5b22c5d83a0329925fb9bef1c25
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2022-01-03 19:01:19 +0100

    update functions describe url all_url

--- lib/souls/version.rb
+++ lib/souls/version.rb
@@ -1,4 +1,13 @@
+require "fileutils"
 module Souls
   VERSION = "1.16.0".freeze
   public_constant :VERSION
+
+  SOULS_PATH =
+    if FileUtils.pwd.split("/").last == "souls"
+      ".".freeze
+    else
+      "#{Gem.dir}/gems/souls-#{Souls::VERSION}".freeze
+    end
+  public_constant :SOULS_PATH
 end
