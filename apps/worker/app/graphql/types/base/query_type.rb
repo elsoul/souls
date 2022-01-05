60352cb9018fc641d2478d2fd39275c32f5f110a
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2022-01-05 20:03:01 +0100

    fix api/worker template

--- apps/worker/app/graphql/types/base/query_type.rb
+++ apps/worker/app/graphql/types/base/query_type.rb
@@ -1,15 +1,4 @@
 module Types
-  class QueryType < SOULs::Types::BaseObject
-    add_field(GraphQL::Types::Relay::NodeField)
-    add_field(GraphQL::Types::Relay::NodesField)
-    workers =
-      Dir["./app/graphql/queries/*.rb"].map do |file|
-        file.gsub("./app/graphql/queries/", "").gsub(".rb", "")
-      end
-    workers.delete("base_query")
-    workers.each do |worker|
-      field worker.underscore.to_s.to_sym,
-            resolver: Object.const_get("Queries::#{worker.singularize.camelize}")
-    end
+  class QueryType < SOULs::Types::QueryObject
   end
 end
