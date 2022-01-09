5730280d4228ce01455d32eb3f9cbd36a4b5ac63
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2022-01-09 17:27:05 +0100

    fix souls mutations

--- lib/souls/app/graphql/souls_mutation.rb
+++ lib/souls/app/graphql/souls_mutation.rb
@@ -1,6 +1,6 @@
 module SOULs
   class SOULsMutation < GraphQL::Schema::RelayClassicMutation
-    def self.post(url:, payload: {}, content_type: "application/json")
+    def post(url:, payload: {}, content_type: "application/json")
       response = Faraday.post(url, payload.to_json, "Content-Type": content_type)
       response.body
     end
@@ -18,7 +18,7 @@ module SOULs
       end
     end
 
-    def self.souls_check_user_permissions(user, obj, method)
+    def souls_check_user_permissions(user, obj, method)
       raise(StandardError, "Invalid or Missing Token") unless user
 
       policy_class = obj.class.name + "Policy"
@@ -27,7 +27,7 @@ module SOULs
       raise(Pundit::NotAuthorizedError, "permission error!") unless permission
     end
 
-    def self.souls_fb_auth(token:)
+    def souls_fb_auth(token:)
       FirebaseIdToken::Certificates.request!
       sleep(3) if ENV["RACK_ENV"] == "development"
       user = FirebaseIdToken::Signature.verify(token)
@@ -36,13 +36,13 @@ module SOULs
       user
     end
 
-    def self.souls_publish_pubsub_queue(topic_name: "send-mail-job", message: "text!")
+    def souls_publish_pubsub_queue(topic_name: "send-mail-job", message: "text!")
       pubsub = Google::Cloud::Pubsub.new(project: ENV["SOULS_GCP_PROJECT_ID"])
       topic = pubsub.topic(topic_name)
       topic.publish(message)
     end
 
-    def self.souls_make_graphql_query(query: "newCommentMailer", args: {})
+    def souls_make_graphql_query(query: "newCommentMailer", args: {})
       if args.blank?
         query_string = %(query { #{query.to_s.underscore.camelize(:lower)} { response } })
       else
@@ -60,27 +60,27 @@ module SOULs
       query_string
     end
 
-    def self.souls_post_to_dev(worker_name: "", query_string: "")
+    def souls_post_to_dev(worker_name: "", query_string: "")
       port = souls_get_worker(worker_name: worker_name)[0][:port]
       endpoint = SOULs.configuration.endpoint
       res = Net::HTTP.post_form(URI.parse("http://localhost:#{port}#{endpoint}"), { query: query_string })
       res.body
     end
 
-    def self.souls_get_worker(worker_name: "")
+    def souls_get_worker(worker_name: "")
       workers = SOULs.configuration.workers
       workers.filter { |n| n[:name] == worker_name }
     end
 
-    def self.souls_auth_check(context)
+    def souls_auth_check(context)
       raise(GraphQL::ExecutionError, "You need to sign in!!") if context[:user].nil?
     end
 
-    def self.production?
+    def production?
       ENV["RACK_ENV"] == "production"
     end
 
-    def self.get_instance_id
+    def get_instance_id
       `curl http://metadata.google.internal/computeMetadata/v1/instance/id -H Metadata-Flavor:Google`
     end
   end
--- spec/app/graphql/souls_mutation_spec.rb
+++ spec/app/graphql/souls_mutation_spec.rb
@@ -1,28 +1,31 @@
 RSpec.describe(SOULs::SOULsMutation) do
   describe "souls_fb_auth" do
     it "should raise ArgumentError with no payload" do
+      cli = SOULs::SOULsMutation.new(object: [], context: [], field: [])
       allow_any_instance_of(FirebaseIdToken::Certificates).to(receive(:request!).and_return(true))
       allow_any_instance_of(FirebaseIdToken::Signature).to(receive(:verify).and_return(nil))
 
       expectval =
         expect do
-          SOULs::SOULsMutation.souls_fb_auth(token: "abc")
+          cli.souls_fb_auth(token: "abc")
         end
 
       expectval.to(raise_error(ArgumentError))
     end
 
     it "should return payload if payload has token" do
+      cli = SOULs::SOULsMutation.new(object: [], context: [], field: [])
       allow_any_instance_of(FirebaseIdToken::Certificates).to(receive(:request!).and_return(true))
       allow_any_instance_of(FirebaseIdToken::Signature).to(receive(:verify).and_return("abc123"))
 
-      result = SOULs::SOULsMutation.souls_fb_auth(token: "test")
+      result = cli.souls_fb_auth(token: "test")
       expect(result).to(eq("abc123"))
     end
   end
 
   describe "publish_pubsub_queue" do
     it "should call publish" do
+      cli = SOULs::SOULsMutation.new(object: [], context: [], field: [])
       allow(Google::Cloud::Pubsub).to(receive(:new).and_return(Google::Cloud::Pubsub::Project.new("abc")))
       allow_any_instance_of(Google::Cloud::Pubsub::Project).to(
         receive(:topic).and_return(
@@ -30,66 +33,73 @@ RSpec.describe(SOULs::SOULsMutation) do
         )
       )
       allow_any_instance_of(Google::Cloud::Pubsub::Project).to(receive(:publish).and_return(true))
-      result = SOULs::SOULsMutation.souls_publish_pubsub_queue(topic_name: "send-mail-job", message: "text!")
+      result = cli.souls_publish_pubsub_queue(topic_name: "send-mail-job", message: "text!")
       expect(result).to(eq(true))
     end
   end
 
   describe "make_graphql_query" do
     it "should make query string from string and arguments" do
+      cli = SOULs::SOULsMutation.new(object: [], context: [], field: [])
       expected_result = "query { mailer(key: \"value\" key2: \"value2\" ) { response } }"
-      result = SOULs::SOULsMutation.souls_make_graphql_query(query: "mailer", args: { key: "value", key2: "value2" })
+      result = cli.souls_make_graphql_query(query: "mailer", args: { key: "value", key2: "value2" })
       expect(result).to(eq(expected_result))
     end
   end
 
   describe "souls_post_to_dev" do
     it "should send post request to development" do
-      allow(SOULs::SOULsMutation).to(receive(:souls_get_worker).and_return([{ port: "4000" }]))
+      cli = SOULs::SOULsMutation.new(object: [], context: [], field: [])
+      allow(cli).to(receive(:souls_get_worker).and_return([{ port: "4000" }]))
       allow(Net::HTTP).to(receive(:post_form).and_return(Net::HTTPResponse.new(1.0, 200, "OK")))
       allow_any_instance_of(Net::HTTPResponse).to(receive(:body).and_return("abc123"))
 
-      expect(SOULs::SOULsMutation.souls_post_to_dev).to(eq("abc123"))
+      expect(cli.souls_post_to_dev).to(eq("abc123"))
     end
   end
 
   describe "souls_get_worker" do
     it "should search for worker from configuation" do
-      result = SOULs::SOULsMutation.souls_get_worker(worker_name: "abc123")
+      cli = SOULs::SOULsMutation.new(object: [], context: [], field: [])
+      result = cli.souls_get_worker(worker_name: "abc123")
       expect(result).to(eq([]))
     end
   end
 
   describe "souls_auth_check" do
     it "should raise error if no user" do
+      cli = SOULs::SOULsMutation.new(object: [], context: [], field: [])
       expectresult =
         expect do
-          SOULs::SOULsMutation.souls_auth_check({})
+          cli.souls_auth_check({})
         end
       expectresult.to(raise_error(GraphQL::ExecutionError))
     end
 
     it "should not raise error if user" do
-      expect(SOULs::SOULsMutation.souls_auth_check({ user: "test" })).to(eq(nil))
+      cli = SOULs::SOULsMutation.new(object: [], context: [], field: [])
+      expect(cli.souls_auth_check({ user: "test" })).to(eq(nil))
     end
   end
 
   describe "production?" do
     it "should return false when not production" do
-      expect(SOULs::SOULsMutation.production?).to(eq(false))
+      cli = SOULs::SOULsMutation.new(object: [], context: [], field: [])
+      expect(cli.production?).to(eq(false))
     end
   end
 
   describe "get_instance_id" do
     it "should curl instance ID" do
-      allow(SOULs::SOULsMutation).to(receive(:`).and_return(true))
-      expect(SOULs::SOULsMutation).to(
+      cli = SOULs::SOULsMutation.new(object: [], context: [], field: [])
+      allow(cli).to(receive(:`).and_return(true))
+      expect(cli).to(
         receive(:`).with(
           "curl http://metadata.google.internal/computeMetadata/v1/instance/id -H Metadata-Flavor:Google"
         )
       )
 
-      expect(SOULs::SOULsMutation.get_instance_id).to(eq(true))
+      expect(cli.get_instance_id).to(eq(true))
     end
   end
 end
