cc19f1419a69eccfd3e0e3819afa6b7423758b71
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2022-08-31 13:21:53 +0200

    add restart

--- lib/souls/cli/docker/index.rb
+++ lib/souls/cli/docker/index.rb
@@ -3,14 +3,14 @@ module SOULs
     desc "psql", "Run PostgreSQL13 Docker Container"
     def psql
       system(
-        "docker run --rm -d \
+        "docker run --restart always -d \
           --name souls-psql \
           -p 5433:5432 \
           -v postgres-tmp:/var/lib/postgresql/data \
           -e POSTGRES_USER=postgres \
           -e POSTGRES_PASSWORD=postgres \
           -e POSTGRES_DB=souls_test \
-          postgres:13-alpine"
+          postgres:14-alpine"
       )
       system("docker ps")
     end
@@ -18,7 +18,7 @@ module SOULs
     desc "mysql", "Run MySQL Docker Container"
     def mysql
       system(
-        "docker run --rm -d \
+        "docker run --restart always -d \
           --name souls-mysql \
           -p 3306:3306 \
           -v mysql-tmp:/var/lib/mysql \
